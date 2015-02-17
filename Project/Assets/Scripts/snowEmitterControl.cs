f4c3fc9fcba6f58e89bc505c56ee8df553a5188f
illbrb <illbrbgit@gmail.com>
2015-02-17 13:48:22 +0000

    Revert "generic updates + new Build Test type"
    This reverts commit e1ce1f49a9388f386fa21c87c4c7d6de28c16ef4.

--- Project/Assets/Scripts/snowEmitterControl.cs
+++ Project/Assets/Scripts/snowEmitterControl.cs
@@ -6,7 +6,7 @@ public class snowEmitterControl : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-		transform.position = new Vector3 (0.0f, 10.0f, -1.0f);
+		transform.position = new Vector3 (0.0f, 10.0f, -0.5f);
 	}
 	
 	// Update is called once per frame
