53e62d7a63b5ac71f58f6d184ab9908f69f2bbb5
baz1919 <thomasjukes1919@gmail.com>
2015-02-17 09:57:44 +0000

    Update textControl.js

--- Project/Assets/Scripts/textControl.cs
+++ Project/Assets/Scripts/textControl.cs
@@ -5,15 +5,12 @@ public class textControl : MonoBehaviour {
 	
 		void Update ()
 		{
-			if(Input.GetKey(KeyCode.LeftArrow))
-			{
+			if (Input.GetKey(KeyCode.LeftArrow)) {
 				Destroy(gameObject);
 			}
 			
-			if(Input.GetKey(KeyCode.RightArrow))
-			{
-			Destroy(gameObject);
+			if (Input.GetKey(KeyCode.RightArrow)) {
+				Destroy(gameObject);
 			}
 		}
-
 }
