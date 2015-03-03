cc5dd7c74cd7b4a37056239e8a8d1451b207b549
illbrb <illbrbgit@gmail.com>
2015-03-03 13:09:23 +0000

    arrowControl
    arrowControl

--- Project/Assets/Scripts/arrowControl.cs
+++ Project/Assets/Scripts/arrowControl.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class ArrowControl : MonoBehaviour {
 
+	public float arrowSpeed;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		float amountToMove = arrowSpeed * Time.deltaTime;
+		transform.Translate (Vector3.down * amountToMove);
+
+		if (transform.position.y < -7.5f)
+			Destroy (this.gameObject);
 	}
 }
