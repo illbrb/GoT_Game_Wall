a9989abbd59d6543342825bf77698bd6e5b3cce1
illbrb <illbrbgit@gmail.com>
2016-04-20 12:41:24 +0100

    firecontrol

--- Project/Assets/Scripts/CrowControl.cs
+++ Project/Assets/Scripts/CrowControl.cs
@@ -41,7 +41,7 @@ public class CrowControl : MonoBehaviour
 	void Update () 
 	{
 		//Arrow Trigger
-		if (Input.GetKeyDown(KeyCode.P))
+		if (Input.GetKeyDown(KeyCode.F))
 
 			//Fires Arrow
 			Instantiate (ArrowPrefab, transform.position, Quaternion.identity);	
