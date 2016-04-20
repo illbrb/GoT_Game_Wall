9b225e39de564ac276fff2e755efec8e004daf2d
illbrb <illbrbgit@gmail.com>
2016-04-20 12:29:33 +0100

    space

--- Project/Assets/Scripts/CrowControl.cs
+++ Project/Assets/Scripts/CrowControl.cs
@@ -41,7 +41,7 @@ public class CrowControl : MonoBehaviour
 	void Update () 
 	{
 		//Arrow Trigger
-		if (Input.GetKeyDown(KeyCode.R))
+		if (Input.GetKeyDown(KeyCode.Space))
 
 			//Fires Arrow
 			Instantiate (ArrowPrefab, transform.position, Quaternion.identity);	
